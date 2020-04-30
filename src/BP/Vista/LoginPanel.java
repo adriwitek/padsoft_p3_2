bdc18c51677571942d60b1f9ebc027b0a7efd958
GuillermoSolla <62621410+GuillermoSolla@users.noreply.github.com>
2020-04-30 19:46:01 +0200

    LOGIN y REGISTRO ARREGLADOS Y EMPEZADO ADMIN

--- src/BP/Vista/LoginPanel.java
+++ src/BP/Vista/LoginPanel.java
@@ -23,7 +23,7 @@ public class LoginPanel extends JPanel {
 		
 		
 		//Subpanel 1
-		JPanel subP1 = new JPanel(new GridLayout(2,2));
+		JPanel subP1 = new JPanel(new GridLayout(3,3));
 		this.etiquetaUsuario = new JLabel("Usuario:");
 		this.campoUsuario = new JTextField(20);
 		this.etiquetaContrasena = new JLabel("Clave");
@@ -32,21 +32,11 @@ public class LoginPanel extends JPanel {
 		subP1.add(campoUsuario);
 		subP1.add(etiquetaContrasena);
 		subP1.add(campoContrasena);
-		this.add(subP1);
-		
-		
-		//Subpanel2
-		JPanel subP2 = new JPanel();
 		this.botonLogin = new JButton("LogIn");
-		subP2.add(botonLogin);
-		this.add(subP2);
-		
-		
-		//Subpanel3
-		JPanel subP3 = new JPanel();
+		subP1.add(botonLogin);
 		this.botonBack = new JButton("Volver");
-		subP3.add(botonBack);
-		this.add(subP3);
+		subP1.add(botonBack);
+		this.add(subP1);
 		
 		
 	}
