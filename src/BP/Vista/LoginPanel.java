4b71a712d7602db9986407b6f9f0ef8e3e78fe4f
arubio1 <adrian.rubiop@estudiante.uam.es>
2020-04-30 19:56:03 +0200

    aaa

--- src/BP/Vista/LoginPanel.java
+++ src/BP/Vista/LoginPanel.java
@@ -24,30 +24,18 @@ public class LoginPanel extends JPanel {
 		
 		
 		//Subpanel 1
-
 		JPanel subP1 = new JPanel(new GridLayout(3,3));
 		this.etiquetaUsuario = new JLabel("Usuario:");
 		this.campoUsuario = new JTextField(20);
 		this.etiquetaContrasena = new JLabel("Clave");
 		this.campoContrasena = new JPasswordField(20);
-
-		
-		//Subpanel2
-		JPanel subP2 = new JPanel();
 		subP1.add(etiquetaUsuario);
 		subP1.add(campoUsuario);
 		subP1.add(etiquetaContrasena);
 		subP1.add(campoContrasena);
 		this.botonLogin = new JButton("LogIn");
-	
-	
-		
-		
-		//Subpanel3
-		//JPanel subP3 = new JPanel();
 		subP1.add(botonLogin);
 		this.botonBack = new JButton("Volver");
-	
 		subP1.add(botonBack);
 		this.add(subP1);
 		
