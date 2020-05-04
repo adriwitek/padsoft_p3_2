28d2f1a444ad6d47ec853f0afa7c111c358d29d1
arubio1 <adrian.rubiop@estudiante.uam.es>
2020-05-04 18:14:44 +0200

    aa

--- src/BP/Controlador/ControladorLogin.java
+++ src/BP/Controlador/ControladorLogin.java
@@ -96,7 +96,6 @@ public class ControladorLogin implements ActionListener {
 		pUsuario.setNombreUsuario(modelo.getUsuarioConectado().getNombre());
 		pUsuario.setListaProyectos(modelo.getProyectosApoyables(modelo.getUsuarioConectado()));
 		pUsuario.setListaColectivos(modelo.getColectivosDisponibles(modelo.getUsuarioConectado()));
-		System.out.println(modelo.getUsuarioConectado().getAllNotificaciones());
 		pUsuario.setModeloNotificaciones(modelo.getUsuarioConectado().getAllNotificaciones());
 
 	}
